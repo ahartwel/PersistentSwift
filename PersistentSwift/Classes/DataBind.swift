8986faf906c628f8e745f7bb37578d925c2e2ee3
ahartwel <hartwellalex@gmail.com>
2017-02-01 17:10:55 -0500

    added databinding to cache

--- PersistentSwift/Classes/DataBind.swift
+++ PersistentSwift/Classes/DataBind.swift
@@ -1,9 +1,165 @@
 //
 //  DataBind.swift
-//  Pods
+//  DataBind
 //
-//  Created by Alex Hartwell on 2/1/17.
+//  Created by Alex Hartwell on 8/8/16.
+//  Copyright © 2016 Alex Hartwell. All rights reserved.
 //
-//
-
 import Foundation
+import UIKit
+
+
+
+class DataBind<T> {
+    static func getBindee(_ listener: DataBindListener<T>) -> DataBindee<T> {
+        let bindee = DataBindee<T>(callback: listener);
+        return bindee;
+    }
+    
+    static func getBindee(_ listener: DataBindListenerOldAndNew<T>) -> DataBindeeOldAndNew<T> {
+        let bindee = DataBindeeOldAndNew<T>(callback: listener);
+        return bindee;
+    }
+    
+}
+
+class DataBindListener<A> {
+    var callback: (A) -> ()
+    init (listener: @escaping (A) -> ()) {
+        self.callback = listener;
+    }
+}
+
+class DataBindListenerOldAndNew<A> {
+    var callback: (A, A) -> ()
+    init (listener: @escaping (A, A) -> ()) {
+        self.callback = listener;
+    }
+}
+
+
+class DataBindee<T> {
+    weak var listener: DataBindListener<T>?
+    init(callback: DataBindListener<T>) {
+        self.listener = callback;
+    }
+    
+}
+
+class DataBindeeOldAndNew<T> {
+    weak var listener: DataBindListenerOldAndNew<T>?
+    init(callback: DataBindListenerOldAndNew<T>) {
+        self.listener = callback;
+    }
+    
+}
+
+////How to use
+/*
+ 
+ 
+ class Model {
+ var dataBindedInt: DataBindType<Int> = DataBindType<Int>(value: 0);
+ }
+ 
+ class Controller {
+ 
+ var model = Model();
+ 
+ override func loadView() {
+ super.loadView();
+ self.model.dataBindedInt.addBindee({
+ [unowned self] newValue in
+ self.onDataBindedIntChange(newInt: newValue);
+ });
+ }
+ 
+ 
+ func onDataBindedIntChange(newInt: Int) {
+ EWLog.shared.logVerbose("got the new int");
+ }
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ */
+
+
+
+class DataBindType<T> {
+    
+    private var listeners: [DataBindListener<T>] = [];
+    private var listenersOldAndNew: [DataBindListenerOldAndNew<T>] = [];
+    
+    
+    var value: T {
+        didSet {
+            if !dontRun {
+                for bindee in bindees {
+                    bindee.listener?.callback(self.value);
+                }
+                for bindee in bindessOldAndNew {
+                    bindee.listener?.callback(self.value, oldValue);
+                }
+            }
+            self.dontRun = false;
+        }
+    };
+    
+    var bindees: [DataBindee<T>] = [];
+    var bindessOldAndNew: [DataBindeeOldAndNew<T>] = [];
+    var dontRun: Bool = false;
+    
+    init(value: T) {
+        self.value = value;
+    }
+    
+    func addBindee(_ callback: @escaping (_ newValue: T, _ oldValue: T) -> ()) {
+        let listener: DataBindListenerOldAndNew<T> = DataBindListenerOldAndNew<T>(listener: callback);
+        self.listenersOldAndNew.append(listener);
+        let bindee = DataBind.getBindee(listener);
+        self.bindessOldAndNew.append(bindee);
+    }
+    
+    func addBindee(_ callback: @escaping (_ newValue: T, _ oldValue: T) -> (), runListener: Bool) {
+        self.addBindee(callback);
+        if (runListener) {
+            callback(self.value, self.value);
+        }
+    }
+    
+    
+    
+    func addBindee(_ callback: @escaping (T) -> ()) {
+        let listener: DataBindListener<T> = DataBindListener<T>(listener: callback);
+        self.listeners.append(listener);
+        let bindee = DataBind.getBindee(listener);
+        self.bindees.append(bindee);
+    }
+    
+    func addBindee(_ callback: @escaping (T) -> (), runListener: Bool) {
+        self.addBindee(callback);
+        if (runListener) {
+            callback(self.value);
+        }
+    }
+    
+    func set(_ value: T) {
+        self.value = value;
+    }
+    
+    func set(_ value: T, dontRun: Bool) {
+        self.dontRun = dontRun;
+        self.value = value;
+    }
+    
+    
+    func get() -> T {
+        return self.value;
+    }
+    
+    
+}
--- PersistentSwift/Classes/PersistentSwift.swift
+++ PersistentSwift/Classes/PersistentSwift.swift
@@ -5,24 +5,38 @@
 //  Created by Alex Hartwell on 1/28/17.
 //  Copyright © 2017 hartwell. All rights reserved.
 //
-
 import Foundation
+
+
 
 open class PSModelCache {
     
     public static var shared: PSModelCache = PSModelCache();
     public var models: [PSCachedModel.Type] = [];
-    public var cache: [String: [PSCachedModel]] = [:];
+    
+    var dictionaryCache: [String: [String: PSCachedModel]] = [:];
     
     
-    /// get models of type from the cache
+    
+    /// get models of type from the cache as an array
     ///
     /// - Parameter ofType: the type of object to get ex. SubclassCachedModel.self
     /// - Returns: the array of models if they exist, otherwise nil
     public func getModelsFromCache<T: PSCachedModel>(ofType: PSCachedModel.Type) -> [T]? {
-        return self.cache[ofType.modelName] as? [T]
+        if let cache = self.dictionaryCache[ofType.modelName] {
+            let array = Array(cache.values);
+            return array as! [T];
+        }
+        return nil;
     }
     
+    /// get models of a specific type from the cache as a dictionary [Id String: Obj]
+    ///
+    /// - Parameter ofType: the type of models you want ex SubclassCachedModel.self
+    /// - Returns: the dictionary of models if they exist, otherwise nil
+    public func getModelsDictionaryFromCache<T: PSCachedModel>(ofType: PSCachedModel.Type) -> [String: T]? {
+        return self.dictionaryCache[ofType.modelName] as? [String: T];
+    }
     
     
     /// Register model types to the cache
@@ -35,10 +49,12 @@ open class PSModelCache {
     }
     
     
+    
     /// add a model to the cache. It will find the proper cache based on the models type, append it and save the cache
     ///
-    /// - Parameter model: the model to save
-    public func addModelToCache(model: PSCachedModel) {
+    /// - Parameter model: the model to add
+    /// - Returns: true if model was added to the cache, false if it was already in it
+    public func addModelToCache(model: PSCachedModel) -> Bool {
         let type: PSCachedModel.Type = type(of: model);
         
         var inside: Bool = false;
@@ -49,27 +65,45 @@ open class PSModelCache {
         }
         if inside == false {
             assertionFailure("You did not register the model type \(type.modelName)");
-            return;
+            return false;
         }
+        
         
         let name = type.modelName;
-        if var cache = self.cache[name] {
-            cache.append(model);
-        } else {
-            self.cache[name] = [];
-            self.cache[name]?.append(model);
-        }
+        
+        self.createCacheIfNeeded(ofName: name);
+        var alreadyInCache: Bool = self.isObjectInCache(ofName: name, obj: model);
+        self.appendObjectToCache(ofName: name, obj: model);
         
         model.isInCache = true;
-        self.saveCache();
+        
+        return alreadyInCache == false;
+    }
+    
+    
+    func isObjectInCache(ofName name: String, obj: PSCachedModel) -> Bool {
+        if (self.dictionaryCache[name]![obj.id] != nil) {
+            return true;
+        }
+        return false;
+    }
+    
+    func createCacheIfNeeded(ofName name: String) {
+        if self.dictionaryCache[name] == nil {
+            self.dictionaryCache[name] = [:];
+        }
+    }
+    
+    func appendObjectToCache(ofName name: String, obj: PSCachedModel) {
+        self.dictionaryCache[name]?[obj.id] = obj;
     }
     
     /// load everything in the cache
     public func loadCache() {
         for model in self.models {
             if let data = UserDefaults.standard.object(forKey: model.modelName) as? Data {
-                if let objs = NSKeyedUnarchiver.unarchiveObject(with: data) as? [PSCachedModel] {
-                    self.cache[model.modelName] = objs;
+                if let objs = NSKeyedUnarchiver.unarchiveObject(with: data) as? [String: PSCachedModel] {
+                    self.dictionaryCache[model.modelName] = objs;
                 }
             }
             
@@ -79,9 +113,7 @@ open class PSModelCache {
     /// save everything in the cache
     public func saveCache() {
         for model in self.models {
-            if self.cache[model.modelName] == nil {
-                self.cache[model.modelName] = [];
-            }
+            self.createCacheIfNeeded(ofName: model.modelName);
             
             //look into removing duplicate objects
             //            let ar = $.uniq(self.cache[model.modelName]!, by: {
@@ -89,7 +121,7 @@ open class PSModelCache {
             //            });
             
             
-            let data = NSKeyedArchiver.archivedData(withRootObject: self.cache[model.modelName]!);
+            let data = NSKeyedArchiver.archivedData(withRootObject: self.dictionaryCache[model.modelName]!);
             UserDefaults.standard.setValue(data, forKeyPath: model.modelName);
             
         }
@@ -97,11 +129,139 @@ open class PSModelCache {
     }
     
     
+    public func clearCache() {
+        self.dictionaryCache = [:];
+    }
+    
 }
 
 
+open class PSDataManager<T: PSCachedModel> {
+    
+    
+    
+    open static func getModelsArray() -> [T] {
+        return T.models as! [T];
+    }
+    
+    open static func getModelsDictionary() -> [String: T] {
+        return T.modelsDictionary as! [String : T];
+    }
+    
+    open static func getModel(byId id: String) -> T? {
+        return T.getModel(byId: id) as? T
+    }
+    
+    open static func getModels<V: Equatable where T: AnyObject>(byValue value: Any, forKey key: String, ofType type: V.Type) -> [T] {
+        let allModels = T.models as! [T];
+        var foundModels: [T] = [];
+        for model in allModels {
+            if let v = model.value(forKey: key) {
+                if let equal = isEqual(type: type, a: v, b: value) {
+                    if equal {
+                        foundModels.append(model);
+                    }
+                }
+            }
+        }
+        return foundModels;
+        
+    }
+    
+    
+    
+    static func isEqual<T: Equatable>(type: T.Type, a: Any, b: Any) -> Bool? {
+        guard let a = a as? T, let b = b as? T else { return nil }
+        
+        return a == b
+    }
+    
+}
+
+
+/// An Enum outlining different data events that can happen and exposing functions for hooking into and calling these events
+///
+/// - none: the starting value
+/// - newDataAdded: call when you want to add data to the data store
+/// - dataUpdated: call when you want to update data in the datastore
+/// - removedData: call when you want to remove data in the datastore(may not be complete, not tested)
+public enum PSDataEvent<T: PSCachedModel> {
+    case none
+    case newDataAdded(T)
+    case dataUpdated(T)
+    
+    /// get the data associated with the event
+    ///
+    /// - Returns: returns the added, updated, or removed data
+    public func getData() -> T? {
+        switch self {
+        case .none:
+            return nil;
+            break;
+        case .newDataAdded(let data):
+            return data;
+            break;
+        case .dataUpdated(let data):
+            return data;
+            break;
+        
+        }
+    }
+    
+    public func isDataAdded() -> Bool {
+        switch self {
+        case .newDataAdded(_):
+            return true;
+            break;
+        default:
+            return false;
+            break;
+        }
+    }
+    public func isDataUpdated() -> Bool {
+        switch self {
+        case .dataUpdated(_):
+            return true;
+            break;
+        default:
+            return false;
+            break;
+        }
+    }
+    
+    /// add data to the data store
+    ///
+    /// - Parameters:
+    ///   - data: the object to add
+    ///   - json: the json to populate the object with
+    ///   - eventHandler: the event handler that will alert other objects about the addition
+    static func addData(_ data: T, eventHandler: inout DataBindType<PSDataEvent>) {
+        eventHandler.set(.newDataAdded(data));
+    }
+    
+    
+    /// update data in the data store
+    ///
+    /// - Parameters:
+    ///   - data: the object to update
+    ///   - json: the json to populate the object with
+    ///   - eventHandler: the event handler that will alert other objects about the update
+    static func updateData(_ data: T, eventHandler: inout DataBindType<PSDataEvent>) {
+        eventHandler.set(.dataUpdated(data));
+    }
+    
+    
+    
+}
+
 /// Base cached model
 @objc open class PSCachedModel: NSObject, NSCoding {
+    
+    static var eventHandler: DataBindType<PSDataEvent<PSCachedModel>> = DataBindType<PSDataEvent<PSCachedModel>>(value: .none);
+    
+    open static func addCallbackOnCacheChange(_ callback: @escaping (PSDataEvent<PSCachedModel>) -> ()) {
+        self.eventHandler.addBindee(callback);
+    }
     
     
     /// The name of the model (used in the model cache)
@@ -111,6 +271,8 @@ open class PSModelCache {
             return "Cached Model"
         }
     }
+    
+    
     
     /// helper property, it gets all of the cached models of this type from the model cache
     open class var models: [PSCachedModel] {
@@ -122,6 +284,26 @@ open class PSModelCache {
         }
     }
     
+    open class var modelsDictionary: [String: PSCachedModel] {
+        get {
+            if let models = PSModelCache.shared.getModelsDictionaryFromCache(ofType: self) {
+                return models;
+            }
+            return [:];
+        }
+    }
+    
+    
+    open class func getModel(byId id: String) -> PSCachedModel? {
+        if let models = PSModelCache.shared.getModelsDictionaryFromCache(ofType: self) {
+            if let model = models[id] {
+                return model;
+            }
+        }
+        return nil;
+    }
+    
+    
     //I am assuming every model has an id (this property is not used in the internals of the cache, you can set it to whatever is in your db)
     public var id: String = "";
     
@@ -130,36 +312,70 @@ open class PSModelCache {
     
     public func encode(with aCoder: NSCoder) {
         let mirror = Mirror(reflecting: self);
-        for child in mirror.children { //mirror the object so we can loop through the object's properties and get the saved values
-            if let name = child.label {
-                aCoder.encode(child.value as? AnyObject, forKey: name);
-            }
-        }
+        self.encodeFromMirror(mirror: mirror, aCoder: aCoder);
     }
     
+    func encodeFromMirror(mirror: Mirror, aCoder: NSCoder) {
+        for child in mirror.children { //mirror the object so we can loop through the object's properties and get the saved values
+            if let name = child.label {
+                aCoder.encode(child.value as? Any, forKey: name);
+            }
+        }
+        if let parent = mirror.superclassMirror {
+            self.encodeFromMirror(mirror: parent, aCoder: aCoder);
+        }
+    }
     
     
     public override init() {
         super.init();
     }
     
-    /// Add the model to the cache
-    public func addToCache() {
+    /// add a model to the cache
+    ///
+    /// - Returns: returns true if the model was added to the cache, false if it was already in the cache
+    public func addToCache() -> Bool {
         if self.isInCache == true {
-            return;
+            return false;
         }
-        PSModelCache.shared.addModelToCache(model: self);
+        
+        let addedToCache = PSModelCache.shared.addModelToCache(model: self);
+        if addedToCache {
+            PSDataEvent.addData(self, eventHandler: &type(of: self).eventHandler);
+        } else {
+            PSDataEvent.updateData(self, eventHandler: &type(of: self).eventHandler);
+        }
+        
+        return addedToCache;
     }
     
     
     required public init?(coder aDecoder: NSCoder) {
         super.init();
         let mirror = Mirror(reflecting: self);
+        self.initFromMirror(mirror: mirror, aDecoder: aDecoder);
+    }
+    
+    func initFromMirror(mirror: Mirror, aDecoder: NSCoder) {
         for child in mirror.children { //mirror the object so we can loop through the object'ss properties and get the saved values
             if let name = child.label {
-                let value = aDecoder.decodeObject(forKey: name);
-                setValue(value, forKey: name);
+                
+                if aDecoder.containsValue(forKey: name) {
+                    
+                    if let value = aDecoder.decodeObject(forKey: name) as? Any {
+                        if value is NSNull {
+                            print("the value for \(name) was NSNull, we are not loading it from the cache");
+                        } else {
+                            setValue(value, forKey: name);
+                        }
+                    }
+                } else {
+                    print("There was no value in key \(name). We are not loading it from the cache");
+                }
             }
+        }
+        if let parent = mirror.superclassMirror {
+            self.initFromMirror(mirror: parent, aDecoder: aDecoder);
         }
     }
     
